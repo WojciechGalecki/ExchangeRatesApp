77a31105f90c3143f281c460be45bd2b3e60cb6d
Wojtek <wojciech.galecki@wp.pl>
2018-05-09 21:56:29 +0200

    main view was created

--- src/main/java/wg/Main.java
+++ src/main/java/wg/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
         primaryStage.setTitle("My Exchange Rates");
-        primaryStage.setScene(new Scene(root, 600, 500));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
